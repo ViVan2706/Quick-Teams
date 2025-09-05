4e606f07a2d9b70efc71266b04c52a67c9dc34b7
Mohit Jaint <imohitjaint@gmail.com>
2025-09-05 16:38:05 +0530

    deleted all the prev code

--- app/lib/demoUser.js
+++
@@ -1,9 +0,0 @@
-// demoUser.js
-export const demoUser = {
-  uid: "u000",
-  email: "demo@example.com",
-  name: "Demo User",
-  role: "Explorer",
-  skills: ["React", "Node.js", "Teamwork"],
-  gender: "Other"
-};
