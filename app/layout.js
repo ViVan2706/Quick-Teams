6c231c1c3c8ebd223798db3295bdaf70471f4e7f
PARTH DUDHE <parthdudhe06@gmail.com>
2025-09-05 15:50:54 +0530

    feat: added demo users

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,8 +21,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       {children}
       </body>
     </html>
   );
 }
+
+
